090fc7a5c95eb4396d8e3346c578ab61d8dbb35f
saulsneider7 <saulsneider@hotmail.com>
2023-05-20 10:04:58 -0500

    preload y metatags

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,19 @@
-import { EnviarCorreo,EnviarWhatsapp,Llamar,IrWeb } from "./funciones.js";
+import { EnviarCorreo, EnviarWhatsapp, Llamar, IrWeb } from "./funciones.js";
+
+/**
+   * Preloader
+   */
+const preloader = document.querySelector('#preloader');
+if (preloader) {
+    window.addEventListener('load', () => {
+        setTimeout(() => {
+            preloader.classList.add('loaded');
+        }, 1000);
+        setTimeout(() => {
+            preloader.remove();
+        }, 2000);
+    });
+}
 
 let whatsapp = document.querySelector("#BtnWhatsapp");
 let email = document.querySelector("#BtnGmail");
@@ -6,6 +21,6 @@ let phone = document.querySelector("#BtnPhone");
 let web = document.querySelector("#BtnpageWeb");
 
 whatsapp.addEventListener("click", EnviarWhatsapp);
-email.addEventListener("click",EnviarCorreo);
-phone.addEventListener("click",Llamar);
-web.addEventListener("click",IrWeb);
+email.addEventListener("click", EnviarCorreo);
+phone.addEventListener("click", Llamar);
+web.addEventListener("click", IrWeb);
